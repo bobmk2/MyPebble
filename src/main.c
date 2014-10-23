f8b19c4b4ebc37deb2a6ad8b7ba6f85c18867991
BobMk2 <too.good.to.see.0806@gmail.com>
2014-10-24 06:03:28 +0900

    表示物を仮で埋めた

--- src/main.c
+++ src/main.c
@@ -11,7 +11,11 @@ static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 static GFont s_weather_font;
 
+// 矩形とか書くようのレイヤー
+static Layer *s_over_layer;
 
+static TextLayer *s_home_chance_layer; // 自宅の降水確率
+static TextLayer *s_work_chance_layer; // 職場の降水確率
 
 static void update_time() {
   // Get a tm structure
@@ -47,20 +51,77 @@ static void update_time() {
 	}
 }
 
-static void main_window_load(Window *window) {
+// 降水確率の文字列初期化
+static void init_rain_chance_texts(Window *window){
+	s_home_chance_layer = text_layer_create(GRect(9, 17, 47, 28));
+	text_layer_set_background_color(s_home_chance_layer, GColorClear);
+  text_layer_set_text_color(s_home_chance_layer, GColorBlack);
+	text_layer_set_text_alignment(s_home_chance_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_home_chance_layer, "99%");
+//	text_layer_set_font(s_home_chance_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PIXEL_MPLUS10_REGULAR_46)));
+	text_layer_set_font(s_home_chance_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_26)));
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_home_chance_layer));
+	
+
+}
+
+
+// エリアの内側を埋める
+static void draw_fill_areas( GContext* ctx, int x, int y, int value ) {
+		for(int i = 0; i < 9; i ++){
+			int t = 1 << i;
+			
+			int a = (i % 3) * 6;
+			int b = (i / 3) * 6;
+			
+			if ( value & t ) {
+				graphics_fill_rect(ctx, GRect( x + a,y + b,3,3), 0, GCornerNone);
+			}
+		}
+}
+
+// 降水量を埋める
+static void draw_fill_amounts( GContext* ctx, int x, int y, int amount) {
+	for( int i = 0; i < amount; i++ ){
+		graphics_fill_rect(ctx, GRect( x + (i*5), y, 4, 6), 0, GCornerNone);
+	}
+}
+
+static void draw_test(Layer *layer, GContext* ctx) {
+	
+	time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+	
+	
+	// 降水確率は既に記述済みとする
+	draw_fill_areas(ctx, 64,35, 341);
+	
+	draw_fill_amounts( ctx, 112, 31, 1 );
+	draw_fill_amounts( ctx, 112, 40, 3 );
+	draw_fill_amounts( ctx, 112, 49, 5 );
+	
+}
+
+
+static void main_window_load(Window *window) {	
 	// Create GBitmap, then set to created BitmapLayer
 	s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_RAIN_SIGHTS_BACKGROUND);
 	s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
 	bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
 	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
 
-
+	s_over_layer = layer_create(GRect(0,0,144,168));
+	layer_set_update_proc( s_over_layer, &draw_test);
+	layer_add_child(window_get_root_layer(window), s_over_layer);
 	
 	// Create time TextLayer
   s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
+	
+	// 降水確率の文字列初期化
+	init_rain_chance_texts(window);
 
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
@@ -109,6 +170,9 @@ bitmap_layer_destroy(s_background_layer);
 	// Destroy weather elements
 text_layer_destroy(s_weather_layer);
 fonts_unload_custom_font(s_weather_font);
+	
+	text_layer_destroy(s_home_chance_layer);
+	layer_destroy(s_over_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -144,6 +208,9 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     // Look for next item
     t = dict_read_next(iterator);
   }
+	
+	// 自宅情報の記述
+	text_layer_set_text(s_home_chance_layer, "50%");
 
 	// Assemble full string and display
 	snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
@@ -187,6 +254,7 @@ static void init() {
 	app_message_register_inbox_dropped(inbox_dropped_callback);
 	app_message_register_outbox_failed(outbox_failed_callback);
 	app_message_register_outbox_sent(outbox_sent_callback);
+
 	
 	// Open AppMessage
 	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
